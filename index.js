5300b35d53636dfed6dacaadd3e6de7350add909
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-21 19:00:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -95,12 +95,13 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
-   const left = positionToInteger(DODGER.style.left)
-    window.requestAnimationFrame()
-    
-  if (left>0){
-  DODGER.style.top=`${top -4}px`}
-   
+  window.requestAnimationFrame(function() {
+    const left = positionToInteger(DODGER.style.left)
+
+    if (left > 0) {
+      DODGER.style.left = `${left - 4}px`;
+    }
+  })
 }
 
 function moveDodgerRight() {
